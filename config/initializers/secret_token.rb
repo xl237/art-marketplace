8e5c2b63a548e41d853787b9e6b2a88ea87e8c0e
Xuan Ky Le <xl237@cornell.edu>
2014-04-14 22:08:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artify::Application.config.secret_key_base = '0b111ea649eeb4c48b21952a4694cbd51806ff4f63aea1837bbf8a8637fe2eac7d8e36aa89c1dee2c229575d44089572ce07725a1b6427fd7d7c01babd997934'
